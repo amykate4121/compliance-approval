aa1847e6016d0ad078af9466c42e4813f037486c
ec20751 <ec20751@qmul.ac.uk>
2023-09-01 14:32:41 +0100

    Integrating Angular with Auth0

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,46 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {ExamsApiService} from './exams/exams-api.service';
 
+
+import {ExamFormComponent} from './exams/exam-form.component';
+import {RouterModule, Routes} from '@angular/router';
+import {ExamsComponent} from './exams/exams.component';
+
+
+import * as Auth0 from 'auth0-web';
+import {CallbackComponent} from './callback.component';
+
+const appRoutes: Routes = [
+  { path: 'callback', component: CallbackComponent },
+  { path: 'new-exam', component: ExamFormComponent },
+  { path: '', component: ExamsComponent },
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamFormComponent,
+    ExamsComponent,
+    CallbackComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+    ),
   ],
   providers: [ExamsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor() {
+    Auth0.configure({
+      domain: 'dev-na28whcishipj6up.us.auth0.com',
+      audience: 'https://online-exam/',
+      clientID: 'pFKG6ErjOE4crtGf0aXxmSt1aWONQTgp',
+      redirectUri: 'http://localhost:4200/callback',
+      scope: 'openid profile manage:exams'
+    });
+  }
 }
