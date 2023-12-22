27e4fbab4ba5e71df5dc74c079c73d8419f1984a
ec20751 <ec20751@qmul.ac.uk>
2023-12-22 16:42:28 +0000

    syncfusion

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app/app.module';
 
 import { registerLicense } from '@syncfusion/ej2-base';
 
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIfEx3THxbf1xzZFJMYFRbRHZPMyBoS35RdURhWH1ed3RdRmBbV0Bx');
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWH1ccnRcQ2RdU0d3W0Y=');
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
