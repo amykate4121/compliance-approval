b0f7591665f21e45ca650fc9df9de87a8f7238de
ec20751 <ec20751@qmul.ac.uk>
2023-12-22 14:00:37 +0000

    syncfusion

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,20 @@ import { AppModule } from './app/app.module';
 
 import { registerLicense } from '@syncfusion/ej2-base';
 
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIfEx3THxbf1xzZFJMYFRbRHZPMyBoS35RdURhWH1ed3RdRmBbV0Bx');
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWH1ccnRSQmhfWER3WUY=');
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+
+// import { environment } from './environments/environment';
+
+
+
+// if (environment.production) {
+//   enableProdMode();
+// }
+
+
