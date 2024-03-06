7c7a91207788ec0a0a3b47a953f56131c398b29d
ec20751 <ec20751@qmul.ac.uk>
2024-03-06 23:15:31 +0000

    python fix

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app/app.module';
 
 import { registerLicense } from '@syncfusion/ej2-base';
 
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NAaF5cWWdCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWX5edXRTQmJZUEZ2WkY=');
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NAaF1cVGhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEFjWn9YcXBVT2VZUEZ0Xg==');
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
